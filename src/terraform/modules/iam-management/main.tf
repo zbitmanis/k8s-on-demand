2c8c5bfb7f2ec496a1480b5d46f498869cf0d239
andris <andris@z-b.lv>
2026-04-03 21:29:01 +0300

    feat: moved gha iam role to cf due chicken egg problem

--- src/terraform/modules/iam-management/main.tf
+++ src/terraform/modules/iam-management/main.tf
@@ -2,14 +2,8 @@ data "aws_caller_identity" "current" {}
 
 # ── GitHub Actions OIDC provider ─────────────────────────────────────────────
 
-data "tls_certificate" "github_actions" {
+data "aws_iam_openid_connect_provider" "github_actions" {
   url = "https://token.actions.githubusercontent.com"
-}
-
-resource "aws_iam_openid_connect_provider" "github_actions" {
-  url             = "https://token.actions.githubusercontent.com"
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.github_actions.certificates[0].sha1_fingerprint]
 }
 
 # ── platform-terraform-execution ─────────────────────────────────────────────
